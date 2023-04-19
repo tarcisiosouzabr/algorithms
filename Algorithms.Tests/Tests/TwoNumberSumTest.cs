1800719699cfe597d15189cf6f5305537c394f5b
Tarcísio Souza <tarcisiosouzabr@gmail.com>
2023-04-18 23:36:56 -0300

    Initial commit

---
+++ Algorithms.Tests/Tests/TwoNumberSumTest.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.Solutions;
+
+namespace Algorithms.Tests.Tests
+{
+    public class TwoNumberSumTest
+    {
+        [Fact]
+        public void TwoNumberSum_Inputs_ReturnOk()
+        {
+            TwoNumberSum twoNumberSum = new TwoNumberSum();
+
+            var result = twoNumberSum.Solution(new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, 10);
+
+            Assert.NotNull(result);
+            Assert.Equal(10, result.Sum());
+            Assert.Equal(new int[] { -1, 11 }, result.OrderBy(x => x).ToArray());
+        }
+    }
+}
---
+++ Algorithms/Solutions/TwoNumberSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Algorithms.Solutions
+{
+    public class TwoNumberSum
+    {
+        public int[] Solution(int[] array, int targetSum)
+        {
+            HashSet<int> nums = new HashSet<int>();
+            foreach (var num in array)
+            {
+                int potentialMatch = targetSum - num;
+                if (nums.Contains(potentialMatch))
+                    return new int[] { potentialMatch, num };
+                else
+                    nums.Add(num);
+            }
+            return new int[0];
+        }
+    }
+}
